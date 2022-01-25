1a3315cd5e8faedc209219e4f6614ddf064df17a
Igor Kabozin <ikabozin@ronasit.com>
2022-01-25 16:35:09 +0300

    #6 Site log

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\SimproLog;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -11,6 +12,13 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
+        $sitesLoggableType = SimproLog::LOGGABLE_TYPE_SITES;
+        $schedule
+            ->command("simpro:save-simpro-log {$sitesLoggableType}")
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->runInBackground();
+
         $schedule->command('clear:set-password-hash')->hourly();
     }
 

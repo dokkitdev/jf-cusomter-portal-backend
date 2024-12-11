8c654677bdca180d260ed65bd27058f3e5b3a7c6
Ivan Merzlyakov <imerzlyakov@ronasit.com>
2024-12-11 17:35:36 +0300

    NGINT-267: changed email logo

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -30,7 +30,7 @@
                 <tr>
                     <td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;">
                         <a href="" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
-                            <img src="{{asset('img/jf-logo-v1-150.png')}}" alt="James Frew Portal">
+                            <img src="{{asset('img/jf-logo-v2-150.png')}}" alt="James Frew Portal">
                         </a>
                     </td>
                 </tr>

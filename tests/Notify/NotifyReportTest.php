870f65c6b57cc3352ffb905c62852bd97aebc489
Ivan Merzlyakov <imerzlyakov@ronasit.com>
2025-08-21 15:34:25 +0300

    tests: added tests

---
+++ tests/Notify/NotifyReportTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Tests\Notify;
+
+use App\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class NotifyReportTest extends TestCase
+{
+    protected array $requiredOriginStates = [
+    ];
+
+    /*************************************
+     *              SEARCH               *
+     *************************************/
+
+    public function getDataTestCreate(): array
+    {
+        return [
+            [
+                //admin - success
+                'userId' => 101,
+                'statusCode' => Response::HTTP_OK,
+                'responseFixture' => null,
+            ],
+            [
+                'userId' => 102,
+                'statusCode' => Response::HTTP_FORBIDDEN,
+                'responseFixture' => 'search_authorization__not_admin__response.json',
+            ],
+            [
+                'userId' => null,
+                'statusCode' => Response::HTTP_UNAUTHORIZED,
+                'responseFixture' => 'search_authorization__no_auth__response.json',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataTestCreate
+     * @testCase search
+     */
+    public function testSearchAuthorization(?int $userId, int $statusCode, ?string $responseFixture): void
+    {
+        if (isset($userId)) {
+            $this->actingAsById($userId);
+        }
+
+        $response = $this->json('get', '/notify/reports');
+
+        $response->assertStatus($statusCode);
+
+        if ($statusCode !== Response::HTTP_OK) {
+            $this->assertEqualsFixture($responseFixture, $response->json());
+        }
+    }
+
+    public function getDataTestSearch(): array
+    {
+        return [
+            [
+                'filters' => [
+                ],
+                'responseFixture' => 'search__no_filters__response.json',
+            ],
+            [
+                'filters' => [
+                    'all' => 1,
+                ],
+                'responseFixture' => 'search__all__response.json',
+            ],
+            [
+                'filters' => [
+                    'page' => 2,
+                    'per_page' => 1,
+                ],
+                'responseFixture' => 'search__page_per_page__response.json',
+            ],
+            [
+                'filters' => [
+                    'order_by' => 'created_at',
+                    'desc' => 1,
+                ],
+                'responseFixture' => 'search__order_by_desc__response.json',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataTestSearch
+     * @testCase search
+     */
+    public function testSearch(array $filters, string $responseFixture): void
+    {
+        $response = $this->actingAsById(101)->json('get', '/notify/reports', $filters);
+
+        $response->assertOk();
+
+        $this->assertEqualsFixture($responseFixture, $response->json());
+    }
+}
---
+++ tests/Notify/fixtures/NotifyReportTest/search/dump.sql
@@ -0,0 +1,5 @@
+INSERT INTO notify_reports (id, report_type, letters_generated, is_finished, created_at, updated_at) VALUES
+(201, 'warehouse_project', 2, true, '2018-10-10 10:10:10', '2018-10-10 11:10:10'),
+(202, 'warehouse_service', 2, true, '2018-10-10 10:10:11', '2018-10-10 11:10:10'),
+(203, 'warehouse_project', 2, true, '2018-10-10 10:10:12', '2018-10-10 11:10:10'),
+(204, 'warehouse_service', 2, true, '2018-10-10 10:10:13', '2018-10-10 11:10:10');

c8e3ec4f17e2cc24e21273b9a9c0bdc06188933a
Ivan Merzlyakov <imerzlyakov@ronasit.com>
2025-08-28 18:47:29 +0300

    fix: fixed dates rendering

--- app/Modules/Notify/Http/Resources/NotifyCsvReport/NotifyCsvReportResource.php
+++ app/Modules/Notify/Http/Resources/NotifyCsvReport/NotifyCsvReportResource.php
@@ -12,16 +12,13 @@ class NotifyCsvReportResource extends BaseResource
 {
     public function toArray($request): array
     {
-        $createdAt = $this->resource->created_at;
-        $updatedAt = $this->resource->updated_at;
-
         return [
             'id' => $this->resource->id,
             'report_type' => __("notify::notify__csv_reports.report_types.{$this->resource->report_type}"),
             'file_name' => $this->resource->file_name,
             'is_finished' => $this->resource->is_finished,
-            'created_at' => isset($createdAt) ? $createdAt->format(self::DATE_FORMAT) : null,
-            'updated_at' => isset($updatedAt) ? $updatedAt->format(self::DATE_FORMAT) : null,
+            'created_at' => $this->renderDate($this->resource->created_at),
+            'updated_at' => $this->renderDate($this->resource->updated_at)
         ];
     }
 }

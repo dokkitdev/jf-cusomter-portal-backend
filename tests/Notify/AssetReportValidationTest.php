bec727de27920098bcc2b244a17f14e477858ab7
Ivan Merzlyakov <imerzlyakov@ronasit.com>
2025-09-08 05:51:42 +0300

    tests: added tests

--- tests/Notify/AssetReportValidationTest.php
+++ tests/Notify/AssetReportValidationTest.php
@@ -98,4 +98,80 @@ class AssetReportValidationTest extends TestCase
 
         $this->assertEqualsFixture($responseFixture, $response->json());
     }
+
+    /*************************************
+     *        GET SEARCH FILTERS         *
+     *************************************/
+
+    public function getDataTestGetSearchFiltersAuthorization(): array
+    {
+        return [
+            [
+                //admin - success
+                'userId' => 101,
+                'statusCode' => Response::HTTP_OK,
+                'responseFixture' => null,
+            ],
+            [
+                'userId' => 102,
+                'statusCode' => Response::HTTP_FORBIDDEN,
+                'responseFixture' => 'authorization__not_admin__response.json',
+            ],
+            [
+                'userId' => null,
+                'statusCode' => Response::HTTP_UNAUTHORIZED,
+                'responseFixture' => 'authorization__no_auth__response.json',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataTestGetSearchFiltersAuthorization
+     * @testCase get_search_filters
+     */
+    public function testGetSearchFiltersAuthorization(?int $userId, int $statusCode, ?string $responseFixture): void
+    {
+        if (isset($userId)) {
+            $this->actingAsById($userId);
+        }
+
+        $response = $this->json('get', '/notify/asset-reports/validation/search-filters');
+
+        $response->assertStatus($statusCode);
+
+        if ($statusCode !== Response::HTTP_OK) {
+            $this->assertEqualsFixture($responseFixture, $response->json());
+        }
+    }
+
+    public function getDataTestGetSearchFilters(): array
+    {
+        return [
+            [
+                'request' => [],
+                'response_fixture' => 'without_selected_site__response.json'
+            ],
+            [
+                'request' => [
+                    'site_id' => 11,
+                ],
+                'response_fixture' => 'with_selected_site__response.json'
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataTestGetSearchFilters
+     * @testCase get_search_filters
+     */
+    public function testGetSearchFilters(array $request, string $responseFixture): void
+    {
+        $response = $this
+            ->actingAsById(101)
+            ->json('get', '/notify/asset-reports/validation/search-filters', $request);
+
+        $response->assertOk();
+
+        $this->assertEqualsFixture($responseFixture, $response->json());
+    }
 }
